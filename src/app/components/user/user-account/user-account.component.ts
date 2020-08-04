a32c85de4adcb4b925bce19f3011aa38ab497afe
JelenaMedareviccc <medarevicj@gmail.com>
2020-08-04 12:05:31 +0200

    ADD: User account

---
+++ src/app/components/user/user-account/user-account.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { CompanyService } from 'src/app/services/company/company.service';
+import { User } from 'src/app/models/user';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
+
+@Component({
+  selector: 'app-user-account',
+  templateUrl: './user-account.component.html',
+  styleUrls: ['./user-account.component.css']
+})
+export class UserAccountComponent implements OnInit {
+  user: User;
+   userId;
+   username: String;
+   showUser: boolean = false;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    let userData = JSON.parse(localStorage.getItem("userData"));
+    this.userId = userData["id"];
+   this.username = userData["username"];
+   this.fetchData();
+
+    
+  }
+
+  fetchData() {
+   
+      this.userService.getUser(this.userId).subscribe(user =>{
+        this.showUser =true;
+        this.user = user;
+      }, error => {
+        console.log(error);
+      });
+
+    }
+
+    editUser() {
+      this.router.navigate([`edit`], { relativeTo: this.route });
+    }
+
+}
+
